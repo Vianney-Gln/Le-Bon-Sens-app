256ffa28734230dc1cfe7d561d0f9d996a872503
Vianney-Gln <vgeloen@gmail.com>
2022-02-17 17:23:07 +0100

    static structure component shop ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // components
 import Header from "./components/Header";
+import Shop from "./components/Shop";
 // style
 import "./styles/App.scss";
 
 const App = () => (
   <div className="container-app">
     <Header />
+    <Shop />
   </div>
 );
 
