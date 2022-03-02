1be7f137db6ec6e444c275b356a8d65839c20af3
Vianney-Gln <vgeloen@gmail.com>
2022-03-02 10:48:50 +0100

    add a function getNamesProductors into services productors

--- src/services/productors.js
+++ src/services/productors.js
@@ -2,10 +2,28 @@ import axios from "axios";
 
 const url = "http://localhost:3001/api/LeBonSens/productors";
 
+/**
+ * function getting only name and id for productors
+ * @returns array
+ */
+const getNamesProductors = () => {
+  axios.get(url).then((result) => result.data);
+};
+
+/**
+ *function getting infos from one productor by his id
+ * @param {number} id
+ * @returns array
+ */
 const getInfosProductors = (id) =>
   axios.get(`${url}/${id}`).then((result) => result.data);
 
+/**
+ *function getting carrousel from one productor by his id
+ * @param {number} id
+ * @returns array
+ */
 const getCarrouselProductor = (id) =>
   axios.get(`${url}/getCarrousel/${id}`).then((result) => result.data);
 
-export { getInfosProductors, getCarrouselProductor };
+export { getInfosProductors, getCarrouselProductor, getNamesProductors };
