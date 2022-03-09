f5c9d5066b9debd108e9bcd3c377df54225b93d7
Vianney-Gln <vgeloen@gmail.com>
2022-03-09 19:00:55 +0100

    fix bug Modal

--- src/admin/DeleteProduct.jsx
+++ src/admin/DeleteProduct.jsx
@@ -5,20 +5,58 @@ import "../styles/deleteProduct.scss";
 import getProducts from "../services/products";
 //components
 import CardsProducts from "../components/cardsProducts";
+//Modal
+import Modal from "react-modal";
 const DeleteProduct = () => {
   /*----- states -----*/
   const [productsToDelete, setProductsToDelete] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false); //state Modal
+  /* -----Modal -----*/
+  Modal.setAppElement("#root");
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  //style Modal
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "240px",
+      height: "160px",
+    },
+  };
 
   /*----- getting all products on component mounting -----*/
   useEffect(() => {
     getProducts().then((result) => {
       setProductsToDelete(result);
-      console.log(result);
     });
   }, []);
 
   return (
     <>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <p>Etes vous sûr de vouloir supprimer ce produit?</p>
+        <div className="container-buttons-modal">
+          <button type="button" onClick={() => console.log("coucou")}>
+            oui
+          </button>
+          <button type="button">non</button>
+        </div>
+      </Modal>
       <h3>Supprimer un produit</h3>
       <div className="container-products-to-delete">
         <ul className="container-list-products-to-delete">
@@ -30,6 +68,7 @@ const DeleteProduct = () => {
                 productPrice={prod.price}
                 productImage={prod.urlImage}
                 toDelete={true}
+                openModal={openModal}
               />
             ))}
         </ul>
--- src/components/cardsProducts.jsx
+++ src/components/cardsProducts.jsx
@@ -1,70 +1,23 @@
 import React, { useState } from "react";
-//Modal
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
 
 const CardsProducts = ({
   productName,
   productPrice,
   productImage,
   toDelete,
+  openModal,
 }) => {
-  /*-----Modal----- */
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const openModal = () => {
-    setIsOpen(true);
-  };
-  const afterOpenModal = () => {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  };
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  /*----- style Modal ------*/
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      width: "240px",
-      height: "160px",
-    },
-  };
-
   return (
-    <>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <p>Etes vous sûr de vouloir supprimer ce produit?</p>
-        <div className="container-buttons-modal">
-          <button type="button" onClick={console.log("coucou")}>
-            oui
-          </button>
-          <button type="button">non</button>
-        </div>
-      </Modal>
-      <li>
-        <img src={productImage} alt="product" />
-        <span>{productName}</span>
-        <span>{productPrice}</span>
-        {toDelete && (
-          <button onClick={openModal} type="button">
-            supprimer
-          </button>
-        )}
-      </li>
-    </>
+    <li>
+      <img src={productImage} alt="product" />
+      <span>{productName}</span>
+      <span>{productPrice}</span>
+      {toDelete && (
+        <button onClick={openModal} type="button">
+          supprimer
+        </button>
+      )}
+    </li>
   );
 };
 export default CardsProducts;
