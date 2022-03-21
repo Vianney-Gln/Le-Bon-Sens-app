290590be4504972f06df2a6cb728a3ef94b74706
Vianney-Gln <vgeloen@gmail.com>
2022-03-21 15:09:41 +0100

    created form AddRecipes.jsx

--- src/admin/AddRecipe.jsx
+++ src/admin/AddRecipe.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const AddRecipe = () => {
-  return <div>AddRecipe</div>;
+  return (
+    <div className="container-addRecipe">
+      <h3>Ajouter une recette</h3>
+      <form>
+        <label htmlFor="name">
+          <span>nom de la recette:</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="nom de la recette"
+          ></input>
+        </label>
+        <label htmlFor="description">
+          <span>description de la recette</span>
+          <textarea
+            type="text"
+            name="description"
+            placeholder="description de la recette"
+          ></textarea>
+        </label>
+        <label htmlFor="cooking time">
+          <span>temps de cuisson:</span>
+          <input
+            type="number"
+            placeholder="temps de cuisson"
+            name="cooking time"
+          ></input>
+        </label>
+        <label htmlFor="preparation time">
+          <span>temps de préparation</span>
+          <input
+            type="number"
+            placeholder="temps de préparation"
+            name="preparation time"
+          ></input>
+        </label>
+        <label htmlFor="urlImage">
+          <span>url de l'image</span>
+          <input
+            type="text"
+            placeholder="url de l'image"
+            name="urlImage"
+          ></input>
+        </label>
+        <button type="submit">Ajouter</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddRecipe;
