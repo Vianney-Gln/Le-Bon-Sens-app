a34fe033f7c33cbc8b50fe08796021178a5071b9
Vianney-Gln <vgeloen@gmail.com>
2022-04-05 17:38:37 +0200

    created service function createOneEvent and getting data user from form as an object

--- src/admin/FormEvents.jsx
+++ src/admin/FormEvents.jsx
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormEvents = () => {
+  /* ------ states variables ------ */
+  const [dataEvents, setDataEvents] = useState({});
+  /**
+   * function getting all infos from the form and turn them into an object in the state dataEvents
+   * @param {string | number} value
+   * @param {string} key
+   */
+  const getDataEvents = (value, key) => {
+    const newData = dataEvents;
+    newData[key] = value;
+    setDataEvents(newData);
+  };
+
+  const handleFormEvents = (e) => {
+    e.preventDefault();
+    console.log(dataEvents);
+  };
   return (
     <div className="container-formEvents">
       <h3>Ajouter des produits en stock</h3>
-      <form>
+      <form onSubmit={handleFormEvents}>
         <label htmlFor="name">
           <input
             type="text"
             name="name"
             placeholder="nom de l'évènement"
+            onChange={(e) => getDataEvents(e.target.value, "name")}
           ></input>
         </label>
         <label htmlFor="description">
           <textarea
             name="description"
             placeholder="nom de l'évènement"
+            onChange={(e) => getDataEvents(e.target.value, "description")}
           ></textarea>
         </label>
         <label htmlFor="date">
-          <input type="datetime-local" name="date"></input>
+          <input
+            type="datetime-local"
+            name="date"
+            onChange={(e) => getDataEvents(e.target.value, "date")}
+          ></input>
         </label>
         <label htmlFor="urlImage">
           <input
             type="text"
             name="urlImage"
             placeholder="url de l'image"
+            onChange={(e) => getDataEvents(e.target.value, "urlImage")}
           ></input>
         </label>
         <button type="submit">valider</button>
--- src/services/events.js
+++ src/services/events.js
@@ -2,7 +2,19 @@ import axios from "axios";
 
 const baseUrl = "https://le-bon-sens.herokuapp.com";
 
+/**
+ * function getting first 6 events
+ * @returns {array}
+ */
 const getInfosEvents = () =>
   axios.get(`${baseUrl}/api/LeBonSens/events`).then((res) => res.data);
+
+export const createOneEvent = (dataEvents) => {
+  return axios({
+    method: "post",
+    url: baseUrl,
+    data: dataEvents,
+  });
+};
 
 export default getInfosEvents;
