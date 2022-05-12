af816ec650472167851806d4c69442df5df21696
Vianney-Gln <vgeloen@gmail.com>
2022-05-12 15:11:32 +0200

    created basic static form addProductor

--- src/admin/FormProductors.jsx
+++ src/admin/FormProductors.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
-const FormProductors = ({ ...props }) => {
-  return <div>FormProductors</div>;
+const FormProductors = ({ operation }) => {
+  return (
+    <div className="container-addProductors">
+      {operation === "updateProductor" && <h3>Modifier un producteur</h3>}
+      {operation === "addProductor" && <h3>Créer un producteur</h3>}
+
+      <form>
+        <label htmlFor="name">
+          <input type="text" name="name" placeholder="nom du producteur" />
+        </label>
+        <label htmlFor="name">
+          <textarea
+            name="description"
+            placeholder="description du producteur"
+          />
+        </label>
+        <label htmlFor="urlGoogleMap">
+          <input
+            type="text"
+            name="urlGoogleMap"
+            placeholder="entrez l'url google map"
+          />
+        </label>
+        <label htmlFor="urlWebsite">
+          <input
+            type="text"
+            name="urlWebsite"
+            placeholder="entrez l'url du site web du producteur"
+          />
+        </label>
+        <label htmlFor="urlImage1">
+          <input
+            type="text"
+            name="urlImage1"
+            placeholder="entrez l'url de votre image 1"
+          />
+        </label>
+        <label htmlFor="urlImage2">
+          <input
+            type="text"
+            name="urlImage2"
+            placeholder="entrez l'url de votre image 2"
+          />
+        </label>
+        <label htmlFor="urlFaceBook">
+          <input
+            type="text"
+            name="urlFaceBook"
+            placeholder="entrez l'url du urlFaceBook"
+          />
+        </label>
+        <label htmlFor="urlTwitter">
+          <input
+            type="text"
+            name="urlTwitter"
+            placeholder="entrez l'url du twitter"
+          />
+        </label>
+        <button type="submit">Valider</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormProductors;
