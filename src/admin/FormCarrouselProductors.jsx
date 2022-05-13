cf073e8e0fefe6ca9bd79f307809d54722457f23
Vianney-Gln <vgeloen@gmail.com>
2022-05-13 14:37:06 +0200

    static form ok

--- src/admin/FormCarrouselProductors.jsx
+++ src/admin/FormCarrouselProductors.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+//style
+import "../styles/formCarrouselProductors.scss";
 
-const FormCarrouselProductors = () => {
-  return <div>FormCarrouselProductors</div>;
+const FormCarrouselProductors = ({ idProductorToManage }) => {
+  return (
+    <div className="container-formCarrouselProductor">
+      <h3>Gestion des images du carrousel</h3>
+      <form>
+        <label htmlFor="urlImageCarrousel">
+          <input
+            type="text"
+            name="urlImageCarrousel"
+            placeholder="saisissez ici l'url de l'image"
+          ></input>
+        </label>
+        <button type="submit">Valider</button>
+      </form>
+      <div className="container-preview-image">
+        <h3>Aperçu des images du carrousel</h3>
+      </div>
+    </div>
+  );
 };
 
 export default FormCarrouselProductors;
