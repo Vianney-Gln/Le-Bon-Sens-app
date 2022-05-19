4baf174f8f2a8c73ea6ef4fdc031de44a5a47345
Vianney-Gln <vgeloen@gmail.com>
2022-05-19 16:12:58 +0200

    page404 created

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// Style
+import "../styles/page404.scss";
+
+const Page404 = () => {
+  return (
+    <div className="page404">
+      <p className="txt404">404</p>
+      <p className="txt-page-not-found">Désolé cette page est introuvable</p>
+      <button type="button">retour</button>
+    </div>
+  );
+};
+
+export default Page404;
