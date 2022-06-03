9754d7b51643c8cc3ffd1e0c2d62fff952877b04
Vianney-Gln <vgeloen@gmail.com>
2022-06-03 14:24:02 +0200

    deleted test text description home

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -30,21 +30,6 @@ const Shop = () => {
                 </span>
               );
             })}
-          Le Bon Sens a été créé en 2022 à l’occasion de l’installation du
-          Potager Ludique. Cécile et Aurélien ont la volonté de créer un espace
-          de lien entre producteurs bio locaux et les consommateurs, ouvrant
-          leurs fermes aux visites et accueillant avec le sourire chaque client.
-          Nos engagements : - Des fermes bio ou engagées en conversion vers
-          l'agriculture biologique; - Des fermes à taille humaine; - Des
-          produits locaux, frais, de saison; - Des fermes les plus locales
-          possibles selon la disponibilité des produits sur le territoire du
-          Pays d’Ancenis; - Des fermiers qui vendent ensemble leurs produits, en
-          assurant la vente à tour de rôle; - La transparence sur les modes de
-          production des produits proposés en magasin, par des fiches, le
-          dialogue mais aussi par des visites de fermes; - Organisation de
-          « marchés à la ferme » regroupant les différents producteurs qui
-          vendent leurs produits au magasin pour que les consommateurs
-          rencontrent leurs paysans locaux.
         </p>
       )}
       <div className="container-images">
