b323832e2cfa9bf90a0822aaff7775d25cdb0405
Vianney-Gln <vgeloen@gmail.com>
2022-07-24 17:53:07 +0200

    changed baseUrl

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "https://shielded-sea-56340.herokuapp.com";
 
 /**
  * Function sending an email from users to owner
