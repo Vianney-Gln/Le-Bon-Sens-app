00aa96d491cfa2e5db06680a9ed3a5ae3263ae63
Vianney-Gln <vgeloen@gmail.com>
2022-07-24 15:29:31 +0200

    created service function sendMail

---
+++ src/services/sendMail.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001";
+
+/**
+ * Function sending an email from users to owner
+ * @param {object} data
+ * @returns {promise}
+ */
+const sendMail = (data) => {
+  return axios({
+    method: "post",
+    url: `${baseUrl}/api/LeBonSens/sendMail`,
+    dat: data,
+  }).then((result) => result.data);
+};
+
+export default sendMail;
